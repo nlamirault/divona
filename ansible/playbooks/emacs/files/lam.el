449703c54c9fe9469ef311037d76c8d972256f1e
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2013-07-10 17:42:48 +0200

    Init Ansible

---
+++ ansible/playbooks/emacs/files/lam.el
@@ -0,0 +1,8 @@
+;; Emacs for Nicolas Lamirault
+;; (c) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+;;
+
+(setq user-full-name "Nicolas Lamirault")
+(setq user-mail-address "nicolas.lamirault@gmail.com")
+
+
---
+++ ansible/playbooks/emacs/files/python.el
@@ -0,0 +1,7 @@
+;;
+;; Python configuration
+;;
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
