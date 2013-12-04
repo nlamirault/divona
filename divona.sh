412195e479a05395711a65c37e0484d9ebf592f5
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2013-12-04 09:27:06 +0100

    FIX TravisCI

--- divona.sh
+++ divona.sh
@@ -5,4 +5,9 @@ DIVONA_HOME=`pwd`
 echo "--------------------------------------"
 echo "----- Configuration with Ansible -----"
 echo "--------------------------------------"
+cd ansible
+echo "Check valid hosts"
 ansible -c local -m ping all -i ansible_hosts
+
+echo "Configure"
+sudo ansible-playbook -c local -i ansible_hosts setup.yml --extra-vars="user=travis"
