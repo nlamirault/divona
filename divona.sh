43164246d39c7c16f1ade0867387e0d6a6a15ad7
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2013-12-04 10:19:30 +0100

    FIX TravisCI and change Divona script

--- divona.sh
+++ divona.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
 
+if [ $# -ne 1 ]
+then
+    echo "$0 <username>"
+    exit 1
+fi
+
 DIVONA_HOME=`pwd`
 echo "--------------------------------------"
 echo "----- Configuration with Ansible -----"
@@ -10,4 +16,4 @@ echo "Check valid hosts"
 ansible -c local -m ping all -i ansible_hosts
 
 echo "Configure"
-sudo ansible-playbook -c local -i ansible_hosts setup.yml --extra-vars="user=travis"
+sudo ansible-playbook -c local -i ansible_hosts setup.yml --extra-vars="user=$1"
