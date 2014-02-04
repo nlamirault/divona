39a8c7e4cdf933e56c126d2479c80ceb44701a1a
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-02-04 15:17:29 +0100

    Update Python configuration

--- ansible/playbooks/emacs/files/python.el
+++ ansible/playbooks/emacs/files/python.el
@@ -1,6 +1,26 @@
-;;
 ;; Python configuration
 ;;
+;; Copyright (c) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+
+;;; License:
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;    http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+;;
+
+;;; Code:
 
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)
@@ -44,3 +64,14 @@
       (if help (message "%s" help)))))
 
 (add-hook 'post-command-hook 'my-flymake-show-help)
+
+(require 'virtualenvwrapper)
+(setq venv-location "~/.virtualenvs/")
+(venv-initialize-interactive-shells)
+(venv-initialize-eshell)
+(add-hook 'python-mode-hook
+                    (lambda ()
+                                    (hack-local-variables)
+                                                  (venv-workon project-venv-name)))
+(setq-default mode-line-format (cons '(:exec venv-current-name) mode-line-format))
+
