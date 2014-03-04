2f7ec3644bf46edc81c0d1ee9a1f3affe0f2a3ef
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-04 09:02:28 +0100

    FIX Slime setup

--- ansible/playbooks/emacs/files/32_commonlisp.el
+++ ansible/playbooks/emacs/files/32_commonlisp.el
@@ -2,7 +2,7 @@
 ;; Slime configuration
 ;;
 
-(let ((filename "~/quicklisp/slime-helpel.el"))
+(let ((filename "~/Apps/quicklisp/slime-helper.el"))
   (when (file-exists-p filename)
     (load (expand-file-name filename))))
 (setq inferior-lisp-program "/usr/bin/sbcl")
