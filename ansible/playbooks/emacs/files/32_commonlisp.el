1c324841be7a8cc51444b953b6f580bafe930149
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-16 10:07:28 +0100

    Add finish task

--- ansible/playbooks/emacs/files/32_commonlisp.el
+++
@@ -1,8 +0,0 @@
-;;
-;; Slime configuration
-;;
-
-(let ((filename "~/quicklisp/slime-helper.el"))
-  (when (file-exists-p filename)
-    (load (expand-file-name filename))))
-(setq inferior-lisp-program "/usr/bin/sbcl")
