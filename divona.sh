e7180e5cf20f1c5053c958371ef6d7d146c94064
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-25 22:21:48 +0000

    FIX Arch configuration

--- divona.sh
+++ divona.sh
@@ -15,4 +15,4 @@ echo "Check valid hosts"
 ansible -c local -m ping all -i ansible/ansible_hosts
 
 echo "Configure"
-sudo ansible-playbook -c local -i $2 ansible/setup.yml --extra-vars="user=$1"
+ansible-playbook -c local -i $2 ansible/setup.yml --extra-vars="user=$1"
