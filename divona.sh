0328a9f6c44b822860443b58cf92366e72a36f5c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-25 11:09:49 +0100

    Init Arch configuration

--- divona.sh
+++ divona.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# -*- coding: UTF8 -*-
 
 if [ $# -ne 1 ]
 then
@@ -11,9 +10,9 @@ DIVONA_HOME=`pwd`
 echo "--------------------------------------"
 echo "----- Configuration with Ansible -----"
 echo "--------------------------------------"
-cd ansible
+#cd ansible
 echo "Check valid hosts"
-ansible -c local -m ping all -i ansible_hosts
+ansible -c local -m ping all -i ansible/ansible_hosts
 
 echo "Configure"
-sudo ansible-playbook -c local -i ansible_hosts setup.yml --extra-vars="user=$1"
+sudo ansible-playbook -c local -i ansible/ansible_hosts ansible/setup.yml --extra-vars="user=$1"
