f4d250e89f3938fa45883bd26a0dc2ad444be7d4
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-04-12 08:24:20 +0200

    Init playbooks for development environment

---
+++ ansible/playbooks/env-dev/files/init.lisp
@@ -0,0 +1,12 @@
+;;
+;; Common Lisp initialize file
+;;
+
+(load "./quicklisp.lisp")
+
+(handler-case
+    (quicklisp-quickstart:install :path "./quicklisp")
+  (simple-error (e)
+    (format t "Warning: ~s" e)))
+
+(load "./quicklisp/setup.lisp")
