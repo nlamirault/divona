b0816aab4a1bac7549af20258fe9aeb3923783d4
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-07-11 12:43:25 +0200

    Update Arch configuration

--- divona.sh
+++ divona.sh
@@ -12,7 +12,7 @@ echo "----- Configuration with Ansible -----"
 echo "--------------------------------------"
 #cd ansible
 echo "Check local host"
-ansible -c local -m ping all -i ansible/hosts/local
+ansible -c local -m ping all -i ansible/hosts/$2
 
 echo "Configure"
 ansible-playbook -i ansible/hosts/$2 ansible/divona.yml --extra-vars="user=$1"
