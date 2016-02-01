19e4a0584d878b5c46636e82e928dd8451d8551b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-02-01 13:56:29 +0100

    Remove Emacs files
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- ansible/roles/emacs/files/02_encodings.el
+++
@@ -1,9 +0,0 @@
-;;
-;; Coding system
-;; Copyright (c) Nicolas Lamirault <nicolas.lamirault@gmail.com>
-;;
-
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
--- ansible/roles/emacs/files/37_scheme.el
+++
@@ -1,8 +0,0 @@
-;; Scheme configuration
-;;
-
-(require 'geiser-install)
-(setq geiser-active-implementations '(guile))
-;;(setq geiser-impl-installed-implementations '(guile))
-
-(provide 'scheme)
